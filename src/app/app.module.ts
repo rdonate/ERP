a81ebceefd5a8bb026f4437596bb08901d9b3d1f
Rubén Donate Serrano <ruben.donate.serrano@gmail.com>
2018-09-07 13:36:38 +0200

    Create main structure of project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// External Libreries
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// Routes
+import { APP_ROUTING } from './app.routes';
+
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
